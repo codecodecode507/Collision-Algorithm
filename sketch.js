3db28ad0d46a7aacdebab1933af4c4f90a07644b
codecodecode507 <70923954+codecodecode507@users.noreply.github.com>
2020-09-10 19:45:40 -0500

    Collision Algorithm
    This is my Collision Algorithm. Check it out.

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(500,500);
+  fixedRect = createSprite(400, 200, 50, 50);
+  fixedRect.shapeColor = ("orange");
+  movingRect = createSprite(200, 200, 50, 50);
+  movingRect.shapeColor = ("pink");
+}
+
+function draw() {
+  background(0);
+  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+    && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
+    && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
+  movingRect.shapeColor = "red";
+  fixedRect.shapeColor = "red";
+}
+else {
+  movingRect.shapeColor = "green";
+  fixedRect.shapeColor = "green";
+}
+  drawSprites();
+}
